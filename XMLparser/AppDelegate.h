596dab0f221e2f110b62bd88f01d2d9084b6f061
apple <apple@localhost.(none)>
2012-09-26 12:00:19 +0800

    Initial Commit

---
+++ XMLparser/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  XMLparser
+//
+//  Created by apple on 9/26/12.
+//  Copyright (c) 2012 Bruderick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ XMLparser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XMLparser
+//
+//  Created by apple on 9/26/12.
+//  Copyright (c) 2012 Bruderick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
